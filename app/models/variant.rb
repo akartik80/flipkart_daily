dbde7242a6788fb91356f70dd663e9cc9dd041fa
Kartik Arora <akartik80@gmail.com>
2019-11-16 11:52:29 +0530

    Implement add to cart

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -5,4 +5,26 @@ class Variant < ApplicationRecord
   def count
     stock_items.sum(:count)
   end
+
+  def available?(count = 1)
+    count <= self.count
+  end
+
+  def unavailable?(count = 1)
+    !available?(count)
+  end
+
+  def process_for_cart(count)
+    raise Standard.new('Not enough items in inventory') if count > self.count
+
+    selected_stock_items = stock_items.where('count > 0')
+    count_to_subtract = count
+
+    selected_stock_items.each do |stock_item|
+      break if count_to_subtract <= 0
+      min_count = [stock_item.count, count_to_subtract].min
+      stock_item.decrement!(:count, min_count)
+      count_to_subtract = count_to_subtract - min_count
+    end
+  end
 end
--- app/services/cart_service.rb
+++ app/services/cart_service.rb
@@ -1,13 +1,18 @@
 class CartService
-  def initialize(cart = nil, user = nil)
+  def initialize(cart: nil, user: nil)
     raise ArgumentError.new('One of user or cart is required') if user.blank? && cart.blank?
 
     self.cart = cart || user.cart || Cart.create!(user: user)
   end
 
   def add(variant, count)
-    cart_item = cart_items.find_or_create_by!(variant: variant)
-    cart_item.increment!(count: count)
+    raise StandardError.new("Variant #{variant.sku} has only #{variant.count} items available") if variant.unavailable?(count)
+
+    ActiveRecord::Base.transaction do
+      cart_item = cart.cart_items.find_or_create_by!(variant: variant)
+      cart_item.update_attributes!(count: (cart_item.count.presence || 0) + count)
+      variant.process_for_cart(count)
+    end
   end
 
   private
